11f9264c277e8970231941cbe2c30001026a6702
= <505825@vistacollege.nl>
2022-04-20 13:18:43 +0200

    iuyt

---
+++ info.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cybersecurity Kidz</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="media/lock.png" type="image/gif">
+</head>
+<body>
+    <div class="header">
+    <img src="media/cattt.gif" alt="Moving cat gif" />
+    </div>
+    <div>
+    <a class="button" href="home.php">Home</a>
+    <a class="button" href="info.php">Info</a>
+    <a class="button" href="quiz.php">Quiz</a>
+</div>
+
+</body>
+</html>
