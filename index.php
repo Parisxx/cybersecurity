829ddfd8d2258d73e46fcdba1718ff5626be52f2
= <505825@vistacollege.nl>
2022-04-20 13:16:43 +0200

    dgdxb

--- index.php
+++ index.php
@@ -6,10 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cybersecurity Kidz</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="media/lock.png" type="image/gif">
 </head>
 <body>
     <div class="header">
+    <img src="media/cattt.gif" alt="Moving cat gif" />
     </div>
+    <div>
+    <a class="button" href="home.php">Home</a>
+    <a class="button" href="info.php">Info</a>
+    <a class="button" href="quiz.php">Quiz</a>
+</div>
 
 </body>
 </html>
