57049c2b915e133c8135b77ca568744de2848631
= <505825@vistacollege.nl>
2022-04-20 13:22:55 +0200

    hg

--- info.php
+++ info.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,15 +9,17 @@
     <link rel="stylesheet" href="styles.css">
     <link rel="icon" href="media/lock.png" type="image/gif">
 </head>
+
 <body>
     <div class="header">
-    <img src="media/cattt.gif" alt="Moving cat gif" />
+        <img src="media/cattt.gif" alt="Moving cat gif" />
     </div>
     <div>
-    <a class="button" href="home.php">Home</a>
-    <a class="button" href="info.php">Info</a>
-    <a class="button" href="quiz.php">Quiz</a>
-</div>
+        <a class="button" href="home.php">Home</a>
+        <a class="button" href="info.php">Info</a>
+        <a class="button" href="quiz.php">Quiz</a>
+    </div>
 
 </body>
+
 </html>
