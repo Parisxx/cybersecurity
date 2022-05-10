907c1f836f3475f47713d91b2c8da5ab96ec9128
= <=>
2022-05-10 14:09:50 +0200

    ndjo

--- info.php
+++ info.php
@@ -22,14 +22,16 @@
 
     <h1>Information </h1>
     <h3 class="info">Cybersecurity </h3>
-    <p class="info-text">Example text about cyberecurity. Written in an easy way for children to understand.
-        Example text about cyber security. Written in an easy and fun way for children to understand
-        Example text about cyberecurity. Written in an easy way for children to understand.
-        Example text about cyber security. Written in an easy and fun way for children to understand.
-        Example text about cyberecurity. Written in an easy way for children to understand.
-        Example text about cyber security. Written in an easy and fun way for children to understand.
-        Example text about cyberecurity. Written in an easy way for children to understand.
-        Example text about cyber security. Written in an easy and fun way for children to understand.
+    <p class="info-text"> Cybersecurity is the practice of defending anything that has to do with the internet. The reason we need something like this is to protect our 'data'.
+        There are a few things you can do to protect your data and your info. Your password is the most important piece of info you have never give it away! It's also recommended to have a good password that is hard to guess!
+        <img src= "./media/catttttttt.png" width="200" height="200" class="paws"> 
+        When you are on the internet you should also always ask your parents if something isn't clear.
+        Never click on links that appear out of the blue! I dont't know about you but it would probably be something not so fun!
+        On the internet there are enough people who have bad intentions with you! You should always show your parents who you are talking to!
+        Cats have good instinct and can tell when something is wrong.. But things on the internet are totally different that's why we oversee the dangers.
+        We reccomend that you double secure your accounts so that they don't get broken into by an unwanted guest that way you will be more secure and much more safer!
+        Always be careful with every site you go on! They're might be dangerous things on the site you are going to so ask your parents first!
+
     </p>
 
 </body>
